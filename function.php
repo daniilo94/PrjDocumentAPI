7123c41afc846fcc51db4d00616e4e0c76441f4d
Danilo Silva <danilo.silva2794@gmail.com>
2017-08-28 02:07:39 -0300

    primeiro commit

---
+++ function.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <?php
+    include './a.php';
+    
+    $nome_uri = $_POST['nome_uri'];
+    $nome_tabela = $_POST['nome_tabela'];
+    
+    $body_response;
+    $response_ok = '{<br>
+“code”:200,<br>
+“message”:”Ok”<br>
+}
+';
+
+    $size = (count($_POST) - 2) / 2;
+
+    
+
+//    for ($i = 1; $i <= $size; $i++) {
+//        if ($i = !$size) {
+//            
+//        } else {
+//            
+//        }
+//    }
+    ?>
+    <body>
+        <table>
+            <tr>
+                <th>Method</th>
+                <th>URI</th>
+                <th>Body(Request)</th>
+                <th>Database query</th>
+                <th>Body(Response)</th>
+            </tr>
+            <tr>
+                <td width="8%">POST</td>
+                <td width="10%">/<?php echo $nome_uri ?>/</td>
+                <td><?php echo body_request1($size); ?></td>
+                <td width="30%"><?php echo database_query1($size) ?></td>
+                <td><?php echo $response_ok ?></td>
+            </tr>
+            <tr>
+                <td>PUT</td>
+                <td>/<?php echo $nome_uri ?>/</td>
+                <td><?php echo body_request2($size); ?></td>
+                <td><?php echo database_query2($size) ?></td>
+                <td><?php echo $response_ok ?></td>
+            </tr>
+            <tr>
+                <td>PUT</td>
+                <td>/<?php echo $nome_uri ?>/disable/</td>
+                <td><?php echo '{<br>"'.$nome_uri.'Id":1<br>}'; ?></td>
+                <td><?php echo database_query3() ?></td>
+                <td><?php echo $response_ok ?></td>
+            </tr>
+            <tr>
+                <td>GET</td>
+                <td>/<?php echo $nome_uri ?>/</td>
+                <td>-</td>
+                <td><?php echo database_query4($size) ?></td>
+                <td><?php echo body_response1($size) ?></td>
+            </tr>
+            <tr>
+                <td>GET</td>
+                <td>/<?php echo $nome_uri.'?'.$nome_uri.'Id=1'; ?></td>
+                <td>-</td>
+                <td><?php echo database_query5($size); ?></td>
+                <td><?php echo body_request1($size); ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <form method="POST" action="function.php">
+            <label>Nome na URI:</label>
+            <input type="text" name="nome_uri" value="product"/>
+
+            <label>Nome na tabela:</label>
+            <input type="text" name="nome_tabela" value="products"/>
+            <br>
+            <hr>
+            <br>
+
+            <label>Atributo:</label>
+            <label>Valor:</label><br>
+            <div id="atributos">
+                <input type="text" name="atributo1" value="name"/>
+                <input type="text" name="valor1" value="Arroz"/><br>
+            </div>
+            <br>
+            <input type="button" value="+" id="mais">
+            <input type="submit" id="enviar" value="Enviar">
+        </form>
+
+        <script>
+            var num = 2;
+            var div = document.getElementById("atributos");
+
+            $('#mais').click(function () {
+                var atributo = document.createElement("input");
+                var valor = document.createElement("input");
+                var br = document.createElement("br");
+
+                atributo.setAttribute("name", "atributo" + num);
+                valor.setAttribute("name", "valor" + num);
+
+                
+
+                div.appendChild(atributo);
+                div.appendChild(valor);
+                div.appendChild(br);
+
+                num = num+1;
+            });
+        </script>
+    </body>
+</html>
